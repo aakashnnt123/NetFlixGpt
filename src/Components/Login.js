aff82624bd620c9a353abebf0a7667043c5a8cbf
aakashnnt123 <aakash5427896@gmail.com>
2025-01-22 04:18:33 +0530

    commit

---
+++ src/Components/Login.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import Header from './Header';
+
+const Login = () => {
+  return (
+    <div>
+      <Header/>
+      <div className='absolute'>
+      <img src="https://assets.nflxext.com/ffe/siteui/vlv3/e3e9c31f-aa15-4a8f-8059-04f01e6b8629/web/IN-en-20250113-TRIFECTA-perspective_febfa442-23d9-45f3-937e-72f8b971f7a9_large.jpg" alt='background-img'/>
+      </div>
+       <form className='w-3/12  bg-black p-12 absolute mt-24 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80'>
+       <h1 className='font-bold text-3xl py-4'>SignIn</h1>
+        <input type='text' placeholder='  Enter Your Email-Id' className='p-2 my-4 w-full bg-gray-700 rounded-lg'/>
+        <input type='password' placeholder='  Enter Your Password' className='p-2 my-4 w-full bg-gray-700 rounded-lg'/>
+        <button className='py-2 my-4 bg-red-700 w-full rounded-lg'>Sign-In</button>
+       </form>
+      </div>
+  )
+}
+
+export default Login;
